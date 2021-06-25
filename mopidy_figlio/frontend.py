36c3070b68bb6db6fd3b7d4cb6711fe0e1bed1ee
mike1098 <michael.frank@mikefrank.de>
2021-06-25 14:33:42 +0100

    Adds test load playlist by interrupt and say playlist name

--- mopidy_figlio/frontend.py
+++ mopidy_figlio/frontend.py
@@ -2,49 +2,53 @@ import logging
 
 from mopidy import core
 import pykka
+import RPi.GPIO as GPIO
 
 logger = logging.getLogger(__name__) #mopidy_figlio.frontend
 
 class FiglioFrontend(pykka.ThreadingActor, core.CoreListener):
   def __init__(self, config, core):
     super().__init__()
-    import RPi.GPIO as GPIO
     self.core = core
     self.config = config["Figlio"]
     self.playlists = []
-    GPIO.setwarnings(False)
+    global counter
+    counter = 0
+    GPIO.setwarnings(True)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(25, GPIO.IN)
-    logger.info('!!!!!!!!!!!!!!!!! Hello from Figlio Frontend!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    self.reload_playlists()
-    uri = tuple(self.core.playlists.get_uri_schemes().get())
-    logger.info("PLaylist as list:\n {0}\n".format(self.core.playlists.as_list().get()))
-    for playlist in self.core.playlists.as_list().get():
-      self.playlists.append(playlist)
-      logger.info("Playlist as URI: {0}".format(playlist.uri))
+    GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    
+    self.reload_playlists()    
+    GPIO.add_event_detect(25, GPIO.FALLING, callback=self.my_callback, bouncetime=1000)
+
+  def on_start(self):
+    logger.info('!!!!!!!!!!!!!!!!! Figlio Frontend started !!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    
+  def on_stop(self):
+    logger.info('!!!!!!!!!!!!!!!!! Figlio Frontend stopped !!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    GPIO.cleanup()
+
+  def on_failure(self):
+    logger.info('!!!!!!!!!!!!!!!!! Figlio Frontend failed !!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    GPIO.cleanup()
+
+  def my_callback(self,channel):
+    global counter  
     self.core.tracklist.clear()
-    logger.info("Playing {0}".format(self.playlists[0]))
-    tracks = self.core.playlists.get_items(self.playlists[0].uri).get()
-    logger.info("Tracks: {0}".format(tracks))
+    playlist_name="Ich spiele für Dich " + self.playlists[counter].name
+    logger.info("Playing: {0}".format(playlist_name))
+    self.core.tracklist.add(uris=["http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=" +playlist_name+"&tl=de"])
+    tracks = self.core.playlists.get_items(self.playlists[counter].uri).get()
     track_uris = [track.uri for track in tracks]
     logger.info("Track URI: {0}".format(track_uris))
     self.core.tracklist.add(uris=track_uris)
-    self.core.tracklist.add(uris=["http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=Hello&tl=en"])
-    logger.info("URI Schemes: {0}".format(self.core.get_uri_schemes().get()))
-    logger.info("Next TLID: {0}".format(self.core.tracklist.get_next_tlid().get()))
-    self.core.playback.play(tlid=1)
-    
-    GPIO.add_event_detect(25, GPIO.BOTH, callback=self.my_callback)   
-  def my_callback(channel):  
-    if GPIO.input(25):     # if port 25 == 1  
-      logger.info("Rising edge detected on 25")  
-    else:                  # if port 25 != 1  
-      logger.info("Falling edge detected on 25")  
-
+    self.core.playback.play()
+    counter += 1  
+  
   def reload_playlists(self):
     self.playlists = []
     for playlist in self.core.playlists.as_list().get():
       self.playlists.append(playlist)
-      logger.debug(playlist)
+      logger.info("Playlist:{0}".format(playlist))
       self.selected_playlist = 0
-      logger.debug("Found {0} playlists.".format(len(self.playlists)))
+      logger.info("Found {0} playlists.".format(len(self.playlists)))
